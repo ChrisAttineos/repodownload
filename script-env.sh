d96d0a674ec1193b5c7db1c0b8e216a4563e7660
ChrisAttineos <131683016+ChrisAttineos@users.noreply.github.com>
2023-06-01 14:55:58 +0200

    Create script-env.sh

---
+++ script-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo yum install docker -y
+#installation docker compose
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+sudo systemctl start docker 
+sudo systemctl enable docker
+sudo docker-compose up --detach
